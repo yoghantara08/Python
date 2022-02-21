8295c7084a14018753998fd72b6fe3ffab99f1de
Yoghantara <gyoghantara@gmail.com>
2022-02-21 13:03:15 +0800

    Working with numbers & use math modules

---
+++ Beginners/5. Working With Numbers/app.py
@@ -0,0 +1,35 @@
+print(2)
+print(2.321)
+print(-2.321)
+
+# Arithmetic
+print(2 + 3.5)
+print(2 - 3.5)
+print(2 * 5)
+print(10 / 2)
+# '*' and '/' first then '+' or '-' depends on the order and parenthesis (...)
+print(5 + 2 * 5) 
+print((5 + 5) * 5)
+# modulus
+print(10 % 4) # 10 divided by 4 and it will print the remain numbers left
+
+my_num = 10
+print(my_num)
+#convert number to strings
+print(str(my_num + 5) + " Convert number to strings")
+
+# Function
+numb_func = -10
+print(abs(numb_func)) #absolute value
+
+print(pow(3, 5)) #3^5 return value 3 to the power of 5 (3 * 3 * 3 * 3 * 3)
+
+print(max(5,10,6)) #return the highest numbers
+print(min(5,10,6)) #return the lowest numbers
+print(round(2.50)) #round a number (membulatkan angka)
+
+from math import *
+
+print(floor(3.9)) #bulatkan ke bawah
+print(ceil(3.2)) #bulatkan ke atas
+print(sqrt(36)) #square root (akar pangkat 2) 6^2
