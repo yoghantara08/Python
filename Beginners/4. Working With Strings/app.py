0d1775790888263179f74f10fd6dfb1169f8ee63
Yoghantara <gyoghantara@gmail.com>
2022-02-21 11:57:55 +0800

    Working with strings

---
+++ Beginners/4. Working With Strings/app.py
@@ -0,0 +1,19 @@
+# Escape character "\"
+print("Free\nCode\nCamp")
+print("\"Tanda Kutip\"")
+
+# Concatenation (joining two strings together)
+phrase = "This is a phrase"
+print(phrase + " and it's cool")
+
+# Function
+testFunction = "Function"
+print(phrase.isupper()) #check if the string is uppercase (return boolean)
+print(testFunction.upper()) #change the string to uppercase
+print(testFunction.upper().isupper())
+print(len(testFunction)) #calculate the length of the strings
+print(testFunction.index("c")) #return index
+print(testFunction.replace("Function", "Test")) #replace strings
+
+# get individual character in strings
+print(phrase[0])
