527bac44cd5e6a15975cf43f6cb1872e9877caae
Yoghantara <gyoghantara@gmail.com>
2022-02-21 11:29:06 +0800

    Variables & data types (string,number,boolean)

---
+++ Beginners/3. Variables & Data Types/app.py
@@ -0,0 +1,20 @@
+# Variables & Data Types
+
+# String
+String = "Gustut"
+# Number
+Number = 20 
+# Boolean
+Laki_Laki = True 
+
+character_name = "Gustut"
+character_age = "20"
+print("Nama : " + character_name)
+print("Umur : " + character_age + " Tahun")
+
+# Menimpa / Mengganti value dalam variable
+character_name = "Yoghantara"
+character_age = "25"
+print("")
+print("Nama : " + character_name)
+print("Umur : " + character_age + " Tahun")
