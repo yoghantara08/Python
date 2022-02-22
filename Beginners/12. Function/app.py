e3e795b299d90825636b3c81069bd7d702e3d53e
Yoghantara <gyoghantara@gmail.com>
2022-02-22 14:12:58 +0800

    Function

---
+++ Beginners/12. Function/app.py
@@ -0,0 +1,13 @@
+def say_hello(n):
+    print("Hello " + n)
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    print("Hello")
+
+say_hello("Gustut")
+
+def sum(a,b):
+    print(a + b)
+
+sum(2,5)
