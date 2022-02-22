dadb0a5ae28939f082c1336598d04b3319b988a1
Yoghantara <gyoghantara@gmail.com>
2022-02-22 12:33:30 +0800

    Basic calculator using input user

---
+++ Beginners/7. Building a Basic Calculator/app.py
@@ -0,0 +1,16 @@
+
+from ast import parse
+from unittest import result
+
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+#int() convert to integer
+# result = int(num1) + int(num2)
+
+#float() convert to float (decimal)
+result = float(num1) + float(num2)
+
+
+print(result)
