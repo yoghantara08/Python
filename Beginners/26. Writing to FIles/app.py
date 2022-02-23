032ed02883f956b94fbeee42ae966481b65ede5f
Yoghantara <gyoghantara@gmail.com>
2022-02-23 16:38:50 +0800

    append and writing files

---
+++ Beginners/26. Writing to FIles/app.py
@@ -0,0 +1,17 @@
+# file_nama = open("C:/Users/user/Desktop/Python/Beginners/25. Reading Files/nama.txt", "a")
+
+# file_nama.write("Dayu Ratih")
+# file_nama.write("\nPutri")
+
+#w = overwrite everything in the file
+# file_nama = open("C:/Users/user/Desktop/Python/Beginners/26. Writing to Files/nama.txt", "w")
+
+# file_nama.write("Test")
+
+# file_nama.close()
+
+python_file = open("C:/Users/user/Desktop/Python/Beginners/26. Writing to Files/testwrite.py", "w")
+
+python_file.write("print('Hello World')")
+
+python_file.close()
