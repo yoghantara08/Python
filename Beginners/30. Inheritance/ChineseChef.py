af1e051d0e7abf0c1c71ca92dd6c8e9a9f83db75
Yoghantara <gyoghantara@gmail.com>
2022-02-23 19:11:26 +0800

    Inheritance

---
+++ Beginners/30. Inheritance/ChineseChef.py
@@ -0,0 +1,10 @@
+from Chef import Chef
+
+#inheritance
+class ChineseChef(Chef):
+
+    def make_special_dish(self):
+        print("The Chinese Chef makes a Tofu")
+
+    def make_fried_rice(self):
+        print("The chef makes a fried rice")
