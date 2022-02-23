c0a3592854169ab2f2478b76f3115fe164728e98
Yoghantara <gyoghantara@gmail.com>
2022-02-23 18:25:30 +0800

    class & object

---
+++ Beginners/27. Class & Object/app.py
@@ -0,0 +1,8 @@
+from Student import Student
+#from (file) import (class)
+
+student1 = Student("Gustut", "Computer Science", 3.73, False)
+student2 = Student("Yoghantara", "Business", 3.3, True)
+
+print(student1.name)
+print(student2.major)
