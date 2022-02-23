5f7de8d8d7bbe024d8b36938c9c6613b7ca01392
Yoghantara <gyoghantara@gmail.com>
2022-02-23 18:57:35 +0800

    Object Funtions

---
+++ Beginners/29. Object Functions/app.py
@@ -0,0 +1,7 @@
+from Student import Student
+
+student1 = Student("Gustut", "Computer Science", 3.7)
+student2 = Student("Yoghantara", "Computer Science", 3.3)
+
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
