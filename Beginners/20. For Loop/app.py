25cdb987ff041636c058fc1ccf74ef6b0ab4b375
Yoghantara <gyoghantara@gmail.com>
2022-02-23 12:09:24 +0800

    for loop

---
+++ Beginners/20. For Loop/app.py
@@ -0,0 +1,23 @@
+for letter in "Python For Loop":
+    print(letter)
+
+array = ["Andy", "Bobi", "Esta", "Yoda"]
+for arr in array:
+    print(arr)
+
+numbers = [1,2,3,4,5]
+for number in numbers:
+    print(number)
+
+for index in range(3, 10):
+    print(index)
+
+#len() length
+for arr in range(len(array)):
+    print(array[arr])
+
+for i in range(5):
+    if i == 0:
+        print("first Iteration")
+    else:
+        print("not first")
