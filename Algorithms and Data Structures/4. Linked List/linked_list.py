8013f1d52c84faa93be8ccdea1cf22cc86e6f2bc
Yoghantara <gyoghantara@gmail.com>
2022-02-26 19:14:55 +0800

    linked list

---
+++ Algorithms and Data Structures/4. Linked List/linked_list.py
@@ -0,0 +1,36 @@
+class Node:
+    """
+    An object for storing a single node of a linked list
+    Models two attributes - data and the link to the next node in the list
+    """
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+class Linked_List:
+    """
+    Singly Linked List
+    """
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        """
+        Returns the number of nodes in the list
+        Takes O(n) time
+        """
+        current = self.head
+        count = 0
+
+        while current != None:
+            count += 1
+            current = current.next_node
+        return count
