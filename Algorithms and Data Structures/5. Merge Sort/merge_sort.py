25ee3c5e0f8c5519b6c9e47e7f26df9247680dd1
Yoghantara <gyoghantara@gmail.com>
2022-02-26 20:17:38 +0800

    merge_sort

---
+++ Algorithms and Data Structures/5. Merge Sort/merge_sort.py
@@ -0,0 +1,20 @@
+def merge_sort(list):
+    """
+    Sorts a list in ascending order
+    Returns a new sorted list
+
+    Divide: Find the midpoint of the list and divide into sublists
+    Conquer: Recursively sort the sublists created in previous step
+    Combine: Merge the sorted sublists created in previous step
+    """
+
+    if len(list) <= 1:
+        return list
+    
+    left_half, right_half = split(list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
+    
